24655f497a42f4fd88643ec627ce72149e772cc9
The-real-geo <giannini000@gmail.com>
2025-08-27 00:04:31 +0930

    deploy router dom ready for auth_v4

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,6 @@
 // pitchero/src/components/LoginPage.js
 import React, { useState } from "react";
-import { auth } from "../firebase";
+import { auth } from "../firebase.js";
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
